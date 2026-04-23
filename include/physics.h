712efab98fe0892e90b1c8b2d776d0badd7fe04a
Umuthan Özel <ozelumuthan@gmail.com>
2026-04-23 16:33:03 +0300

    Add new material types

--- include/physics.h
+++ include/physics.h
@@ -6,6 +6,18 @@
 
 namespace Physics
 {
+    inline bool is_in_bounds(const World &world, int x, int y)
+    {
+        return x >= 0 && x < world.get_width() && y >= 0 && y < world.get_height();
+    }
+
+    inline bool is_cell_empty(const World &world, int x, int y)
+    {
+        if (!is_in_bounds(world, x, y))
+            return false;
+        return world.get_cell_id(x, y) == MaterialType::EMPTY;
+    }
+
     inline uint32_t fast_rand(uint32_t &state)
     {
         state ^= state << 13;
@@ -14,34 +26,125 @@ namespace Physics
         return state;
     }
 
+    inline void mark_updated(World &world, int x, int y)
+    {
+        if (is_in_bounds(world, x, y))
+        {
+            int index = world.coords_to_index(x, y);
+            const_cast<Cell &>(world.get_cell(index)).update_frame = world.get_world_frame();
+        }
+    }
+
+    inline bool is_updated(const World &world, int x, int y)
+    {
+        if (!is_in_bounds(world, x, y))
+            return false;
+        return world.get_cell(x, y).update_frame == world.get_world_frame();
+    }
+
     inline void update_powder(World &world, int x, int y)
     {
-        if (world.get_cell_id(x, y + 1) == MaterialType::EMPTY)
+        if (is_updated(world, x, y))
+            return;
+
+        if (is_cell_empty(world, x, y + 1))
         {
             world.move_cell(x, y, x, y + 1);
+            mark_updated(world, x, y + 1);
             return;
         }
 
         int direction = (fast_rand(world.rng_state) & 1) ? 1 : -1;
 
-        if (world.get_cell_id(x + direction, y + 1) == MaterialType::EMPTY)
+        if (is_cell_empty(world, x + direction, y + 1))
         {
             world.move_cell(x, y, x + direction, y + 1);
+            mark_updated(world, x + direction, y + 1);
             return;
         }
 
-        if (world.get_cell_id(x - direction, y + 1) == MaterialType::EMPTY)
+        if (is_cell_empty(world, x - direction, y + 1))
         {
             world.move_cell(x, y, x - direction, y + 1);
+            mark_updated(world, x - direction, y + 1);
             return;
         }
     }
 
     inline void update_liquid(World &world, int x, int y)
     {
+        if (is_updated(world, x, y))
+            return;
+
+        if (is_cell_empty(world, x, y + 1))
+        {
+            world.move_cell(x, y, x, y + 1);
+            mark_updated(world, x, y + 1);
+            return;
+        }
+
+        int spread_direction = (fast_rand(world.rng_state) & 1) ? 1 : -1;
+        const int MAX_SPREAD = 3;
+
+        for (int i = 1; i <= MAX_SPREAD; ++i)
+        {
+            int new_x = x + (spread_direction * i);
+
+            if (!is_in_bounds(world, new_x, y))
+                break;
+
+            if (is_cell_empty(world, new_x, y))
+            {
+                world.move_cell(x, y, new_x, y);
+                mark_updated(world, new_x, y);
+                return;
+            }
+
+            if (world.get_cell_id(new_x, y) != MaterialType::EMPTY)
+                break;
+        }
+
+        for (int i = 1; i <= MAX_SPREAD; ++i)
+        {
+            int new_x = x - (spread_direction * i);
+
+            if (!is_in_bounds(world, new_x, y))
+                break;
+
+            if (is_cell_empty(world, new_x, y))
+            {
+                world.move_cell(x, y, new_x, y);
+                mark_updated(world, new_x, y);
+                return;
+            }
+
+            if (world.get_cell_id(new_x, y) != MaterialType::EMPTY)
+                break;
+        }
     }
 
     inline void update_gas(World &world, int x, int y)
     {
+        if (is_updated(world, x, y))
+            return;
+
+        constexpr int directions_y[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+        constexpr int directions_x[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+        int start_dir = fast_rand(world.rng_state) % 8;
+
+        for (int i = 0; i < 8; ++i)
+        {
+            int dir_idx = (start_dir + i) % 8;
+            int new_x = x + directions_x[dir_idx];
+            int new_y = y + directions_y[dir_idx];
+
+            if (is_cell_empty(world, new_x, new_y))
+            {
+                world.move_cell(x, y, new_x, new_y);
+                mark_updated(world, new_x, new_y);
+                return;
+            }
+        }
     }
 }
